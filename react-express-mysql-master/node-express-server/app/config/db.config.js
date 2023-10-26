98f305acd0b0e006b800963f7edfdc6024794856
Péter Petrényi <46268149+Redhgar@users.noreply.github.com>
2023-10-26 19:52:46 +0200

    holá

--- react-express-mysql-master/node-express-server/app/config/db.config.js
+++ react-express-mysql-master/node-express-server/app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   HOST: "localhost",
   USER: "root",
-  PASSWORD: "123456",
-  DB: "testdb",
+  PASSWORD: "1234",
+  DB: "tutorials",
   dialect: "mysql",
   pool: {
     max: 5,
